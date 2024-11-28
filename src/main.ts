693a02b62cad65ee88ea27ef8f5eeb916496e869
Renato Parraguez Rubilar <rena_010@hotmail.com>
2024-11-28 03:44:43 -0300

    the method OPTIONS has been added in the vercel file

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('bootstrap');
   
-  app.enableCors({
-    origin: '*',  // Permite solicitudes desde cualquier dominio
-    methods: 'GET, POST, PUT, DELETE',  // Métodos permitidos
-    allowedHeaders: 'Content-Type, Authorization',  // Cabeceras permitidas
-    credentials: false,  // No es necesario manejar credenciales (como cookies o cabeceras de autenticación)
-  });
+  app.enableCors();
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
